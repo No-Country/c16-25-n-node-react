f180e20732a131fb8d570107434148b19df879c2
Diego Iacub <105656485+sombrerodiego@users.noreply.github.com>
2024-02-15 15:09:03 -0300

    Agregue ProductDetail y Card de ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import ProductDetail from './components/ProductDetail'
 import ProductList from './components/ProductList'
 
 function App() {
@@ -7,7 +8,11 @@ function App() {
   return (
     <>
       <h1>Geek E-commerce</h1>
+      <br />
+      <br />
       <ProductList/>
+      <h3>Detalles del Producto:</h3>
+      <ProductDetail productId="9H1ZbrozIEAmbgHRg3NR" />
     </>
   )
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,41 +4,53 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 const ProductList = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const productsRef = collection(db, "products");
-            const querySnapshot = await getDocs(productsRef);
-            const productsData = querySnapshot.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id
-            }));
-            setProducts(productsData);
-        };
-
-        fetchProducts();
-    }, []);
-
-    return (
-        
-        <div>
-            {products.map((product) => (
-                 <div key={product.id}>
-                     <h1 className="text-3xl font-bold underline">Producto desde Firebase:</h1>
-                     <h1>{product.nombre}</h1>
-                     <p>{product.descripcion}</p>
-                     <p>{product.categoria}</p>
-                     <p>{product.precio}</p>
-                     <p>{product.imagen}</p>
-
-
-                     {/* Mostrar otros datos del libro aquí */}
-                 </div>
-
-            ))}
-            </div>
-        );
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const productsRef = collection(db, "products");
+      const querySnapshot = await getDocs(productsRef);
+      const productsData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setProducts(productsData);
     };
-    
-    export default ProductList;
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="flex flex-wrap">
+      {products.map((product) => (
+        <div key={product.id}>
+          <div className="bg-gradient-to-br from-white to-transparent rounded-lg shadow-lg py-4 px-6 m-6 w-64 h-72 text-left">
+            <span className="text-xs  font-bold text-blue-500">
+              {product.categoria}
+            </span>
+            <h2 className="text-2xl font-bold -my-0.5">{product.nombre}</h2>
+            <p className="text-xs font-bold text-gray-500">
+              {product.descripcion}
+            </p>
+            {/* <div className="flex justify-center mb-4">
+                  <img src="imagen-del-producto.jpg" alt="Producto" className="w-3/4"/>
+                </div> */}
+            <div className="flex justify-center items-center bg-gray-200 h-24 my-4">
+              <span className="text-gray-500 text-5xl">Imagen</span>
+            </div>
+            <div className="flex justify-between items-center mt-10">
+              <span className="text-md font-bold text-gray-500">
+                {product.precio}
+              </span>
+              <button className="text-xs text-gray-300 bg-blue-700 hover:bg-blue-600 text-xxs py-2 px-2 rounded-2xl">
+                Comprar
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
